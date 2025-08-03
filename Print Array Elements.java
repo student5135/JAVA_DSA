9182c2a12eaa86a3b47c823dd9148cebb5018b26
Rohan Adhav <adhavrohan5135@gmail.com>
2025-08-03 23:22:55 +0530

    Print Array Elements.java

---
+++ Print Array Elements.java
@@ -0,0 +1,10 @@
+public class PrintArray {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40, 50};
+
+        System.out.println("Array elements are:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
